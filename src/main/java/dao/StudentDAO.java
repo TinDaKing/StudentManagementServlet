bb7f4bcdae8aaf5e370a464ab8897aa9df381844
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-04-16 15:19:29 +0700

    update tools section

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -13,6 +13,7 @@ import model.Course;
 import model.Student;
 
 public class StudentDAO {
+
 	private static StudentDAO instance;
 
 	public static StudentDAO getInstance() {
@@ -27,9 +28,14 @@ public class StudentDAO {
 
 	private static final String GET_ALL_STUDENTS = "SELECT * FROM studentmanagement.student;";
 	private static final String GET_COURSES_A_STUDENT_JOINED = "SELECT c.* FROM studentcourse s\r\n"
-			+ "JOIN course c on s.class_id = c.class_id\r\n"
-			+ "where s.student_id = ?;";
-
+			+ "JOIN course c on s.class_id = c.class_id\r\n" + "where s.student_id = ?;";
+	private static final String GET_ALL_STUDENTS_SORT_BY = "SELECT * FROM studentmanagement.student\r\n" + "ORDER BY ";
+	private static final String GET_STUDENTS_BY_NAME = "SELECT * FROM studentmanagement.student\r\n"
+			+ "WHERE name LIKE '%";
+	private static final String DELETE_STUDENT_BY_ID = "DELETE FROM student WHERE student_id=?;";
+	private static final String ADD_A_STUDENT = "INSERT INTO student(name,grade,birthday,address,note) VALUES (?,?,?,?,?);";
+	private String prePivot;
+	private String nextOrder;
 
 	public List<Student> getAllStudents() {
 		Connection connection = Connector.makeConnection();
@@ -63,29 +69,51 @@ public class StudentDAO {
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
-				
+
 			}
 		}
 
 		return studentList;
 	}
-	
-	public List<Course> getCoursesStudentJoined(int studentId){
+
+	public List<Student> getAllStudentsSortBy(String pivot) {
 		Connection connection = Connector.makeConnection();
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		List<Course> coursesList = new ArrayList<>();
+		List<Student> studentList = new ArrayList<>();
 		try {
-			ps = connection.prepareStatement(GET_COURSES_A_STUDENT_JOINED);
-			ps.setInt(1, studentId);
+			String query = "";
+			if (prePivot != null && pivot.equals(prePivot)) {
+				query = GET_ALL_STUDENTS_SORT_BY + prePivot + nextOrder;
+				if (nextOrder == " DESC;")
+					nextOrder = " ASC;";
+				else
+					nextOrder = " DESC;";
+			} else {
+				if (pivot.equals("name")) {
+					query = GET_ALL_STUDENTS_SORT_BY + "name;";
+					prePivot = "name";
+					nextOrder = " DESC;";
+				} else if (pivot.equals("grade")) {
+					query = GET_ALL_STUDENTS_SORT_BY + "grade DESC;";
+					prePivot = "grade";
+					nextOrder = " ASC;";
+				} else {
+					query = GET_ALL_STUDENTS_SORT_BY + "student_id;";
+					prePivot = "student_id";
+					nextOrder = " DESC;";
+				}
+			}
+			ps = connection.prepareStatement(query);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				int sid = rs.getInt("class_id");
+				int id = rs.getInt("student_id");
 				String name = rs.getString("name");
-				String lecturer = rs.getString("lecturer");
-				String year = rs.getString("year");
+				double grade = rs.getDouble("grade");
+				Date birthDay = rs.getDate("birthday");
+				String address = rs.getString("address");
 				String note = rs.getString("note");
-				coursesList.add(new Course(studentId, name, name, sid, note));
+				studentList.add(new Student(id, name, grade, birthDay, address, note));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -102,11 +130,154 @@ public class StudentDAO {
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
-				
+
+			}
+		}
+
+		return studentList;
+	}
+
+	public List<Student> getStudentsByName(String nameInput) {
+		Connection connection = Connector.makeConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<Student> studentList = new ArrayList<>();
+		try {
+			ps = connection.prepareStatement(GET_STUDENTS_BY_NAME + nameInput + "%';");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt("student_id");
+				String name = rs.getString("name");
+				double grade = rs.getDouble("grade");
+				Date birthDay = rs.getDate("birthday");
+				String address = rs.getString("address");
+				String note = rs.getString("note");
+				studentList.add(new Student(id, name, grade, birthDay, address, note));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+		}
+
+		return studentList;
+	}
+
+	public List<Course> getCoursesStudentJoined(int studentId) {
+		Connection connection = Connector.makeConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		List<Course> coursesList = new ArrayList<>();
+		try {
+			ps = connection.prepareStatement(GET_COURSES_A_STUDENT_JOINED);
+			ps.setInt(1, studentId);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				int classId = rs.getInt("class_id");
+				String name = rs.getString("name");
+				String lecturer = rs.getString("lecturer");
+				int year = rs.getInt("year");
+				String note = rs.getString("note");
+				coursesList.add(new Course(classId, name, lecturer, year, note));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+
 			}
 		}
 
 		return coursesList;
 	}
 
+	public boolean deleteStudentByID(int studentId) {
+		Connection connection = Connector.makeConnection();
+		PreparedStatement ps = null;
+		int rowChanged = 0;
+		try {
+			ps = connection.prepareStatement(DELETE_STUDENT_BY_ID);
+			ps.setInt(1, studentId);
+			rowChanged = ps.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+		}
+
+		if (rowChanged > 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean addAStudent(String name, double grade, Date birthday, String address, String note) {
+		Connection connection = Connector.makeConnection();
+		PreparedStatement ps = null;
+		int rowChanged = 0;
+		try {
+			ps = connection.prepareStatement(ADD_A_STUDENT);
+			ps.setString(1, name);
+			ps.setDouble(2, grade);
+			ps.setDate(3, birthday);
+			ps.setString(4, address);
+			ps.setString(5, note);
+			rowChanged = ps.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+		}
+
+		if (rowChanged > 0) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/main/webapp/students.jsp
+++ src/main/webapp/students.jsp
@@ -11,7 +11,9 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="icon" href="assets/images/pawprint.png">
-	<title>Student managemnet by Tran Hoang Tin</title>
+	<link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+	<title>Student management by Tran Hoang Tin</title>
 </head>
 <body>
 	<div class="nav-bar">
@@ -20,31 +22,88 @@
 	    <a href="<%=request.getContextPath()%>/courses">
 	    	<button class="button-nav-bar">Courses</button>
 	    </a>    
+	    <a href="<%=request.getContextPath()%>/register">
+	    	<button class="button-nav-bar">Register for course</button>
+	    </a>  
     </div>
-    <table class="table-student">
+    <div class="content-container">
+    
+    	<div class="tool-section">
+	    	
+	    	<form action="students" method="post" >
+	    		<p>Find by name:</p>
+	            <input type="text" class="input-search" name="studentName" placeholder="student name"/>
+				<button type="submit" class="button-search">Search</button>
+	        </form>
+	        
+	        <form action="add_student" method="post" class="add-student">
+	    		<p>Add new student:</p>
+	    		<span>Name</span>
+	            <input type="text" class="input-search" name="name" placeholder="Input name"/>
+	            <span>Grade</span>
+	            <input type="number" step="0.01" class="input-search" name="grade" placeholder="Ex: 8.25"/>
+	            <span>Birthday</span>
+	            <input type="date" class="input-search" name="birthday" placeholder="mm-dd-yyyy"/>
+	            <span>Address</span>
+	            <input type="text" class="input-search" name="address" placeholder="Input address"/>
+	            <span>Note</span>
+	            <input type="text" class="input-search" name="note" placeholder="(optional)"/>
+				<button type="submit" class="button-search">Submit</button>
+	        </form>
+
+	    </div>
+	    
+	    <table class="table-student">
     	<tr>
-    		<th>ID</th>
-    		<th>Name</th>
-    		<th>Grade</th>
+    		<th>ID &nbsp; 
+    			<a href="<%=request.getContextPath()%>/students?sortby=student_id">
+    				<i class="fa-solid fa-sort"></i>
+    			</a>
+    		</th>
+    		<th>Name &nbsp; 
+    			<a href="<%=request.getContextPath()%>/students?sortby=name">
+    				<i class="fa-solid fa-sort"></i>
+    			</a>
+    		</th>
+    		<th>Grade &nbsp; 
+    			<a href="<%=request.getContextPath()%>/students?sortby=grade">
+    				<i class="fa-solid fa-sort"></i>
+    			</a>
+    		</th>
     		<th>Birthday</th>
     		<th>Address</th>
     		<th>Note</th>
     		<th>Detail</th>
+    		<th>Delete</th>
+    		
     	</tr>
+    	
     	<c:forEach var="stu" items="${studentList}">
-    		<hr>
-    		<tr>
+    		
+    		<tr class="add-space">
     			<td>${stu.id}</td>
     			<td>${stu.name}</td>
     			<td>${stu.grade}</td>
     			<td>${stu.birthday}</td>
     			<td>${stu.address}</td>
     			<td>${stu.note}</td>
-    			
-    			<td><a href="<%=request.getContextPath()%>/student-detail?id=${stu.id}" class="close">Detail here</a></td>
+    			<td>
+    				<a href="<%=request.getContextPath()%>/student-detail?id=${stu.id}">
+    					<i class="fa-solid fa-circle-info"></i>
+    				</a>
+    			</td>
+    			<td>
+    				<a href="<%=request.getContextPath()%>/delete_student?id=${stu.id}" onclick="return confirm('Are you sure you want to delete this item?');">
+    					<i class="fa-sharp fa-solid fa-trash"></i>
+    				</a>
+    			</td>
     		</tr>
 	    </c:forEach>
     </table>
     
+    </div>
+    
+    
+   
 </body>
 </html>
